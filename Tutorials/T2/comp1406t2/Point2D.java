c83ddd5c9e59f8544373318f76f89cf08e7852fa
mjhinek <jason.hinek@carleton.ca>
2019-01-22 19:41:05 -0500

    Add files via upload

---
+++ Tutorials/T2/comp1406t2/Point2D.java
@@ -0,0 +1,16 @@
+package comp1406t2;
+
+public class Point2D{
+	// instance attributes (state of objects)
+	double x;
+	double y;
+	
+	
+	// instance methods (behanviour of objects)
+	
+	//
+	// add your methods here
+	//
+
+	
+}
---
+++ Tutorials/T2/comp1406t2/Tutorial02.java
@@ -0,0 +1,34 @@
+package comp1406t2;
+
+public class Tutorial02{
+
+	// an array of strings to use
+	public static String[] words = {"cat", "dog", "eel", "owl", "fox", "kitten", "puppy", "elver", "owlet", "kit"};
+
+	public static void strings(){
+		//
+		// add your code here for part 1
+		//
+	}
+	
+	public static void rollRandomDice(long seed){
+		//
+		// add your code for part 2 here
+		//
+	}
+	
+	
+	public static void testPoint2D(){
+		// 
+		// add your code for part 3 here
+		//
+	}
+	
+	public static void main(String[] args){
+		strings();
+		rollRandomDice(17);
+		testPoint2D();
+	
+	}
+	
+}
