89f118e050bef073273a1196f9c3888001ff564a
mjhinek <jason.hinek@carleton.ca>
2019-07-03 15:58:56 -0400

    Create Find.java

---
+++ Tutorials/T1/comp1406t1/Find.java
@@ -0,0 +1,11 @@
+package comp1406t1;
+
+public class Find{
+
+  public static int[] findString(String target, String[] strings){
+    int[] locations = new int[1]{-1};
+
+    return locations;
+  }
+
+}
