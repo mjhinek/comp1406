789a3b232e62d318f035f8ea195ccf0e9f84da5b
mjhinek <jason.hinek@carleton.ca>
2019-07-08 15:18:12 -0400

    Update Plateaus.java

--- Assignments/A1/comp1406a1/Plateaus.java
+++ Assignments/A1/comp1406a1/Plateaus.java
@@ -8,22 +8,22 @@ package comp1406a1;
   */
 public class Plateaus{
 
-  /** Finds the number of "plateaus" in the input data array.
-    * <p>
-		* Examples:
-		*   int[] data1 = {1,3,3,1,5,5,5,2,10,6};
-		*   numberOfPlateaus(data1) ==> 2 ( 3,3 and 5,5,5 are plateaus)
-		*   int[] data2 = {4,3,6};
-		*   numberOfPlateaus(data2) ==> 0 (there are no plateaus)
-		*   int[] data3 = {2,2};
-		*   numberOfPlateaus(data3) ==> 1 ( 2,2 is a plateau)
-    * @param data is an array of one or more integers.
-    *
-    * @return the number of "plateaus" in the input data array.
-    */
-  public static int numberOfPlateaus(int[] data){
+      /** Finds the number of "plateaus" in the input data array.
+    	* <p>
+    	* Examples:
+	*   int[] data1 = {1,3,3,1,5,5,5,2,10,6};
+	*   numberOfPlateaus(data1) ==> 2 ( 3,3 and 5,5,5 are plateaus)
+	*   int[] data2 = {4,3,6};
+	*   numberOfPlateaus(data2) ==> 0 (there are no plateaus)
+	*   int[] data3 = {2,2};
+	*   numberOfPlateaus(data3) ==> 1 ( 2,2 is a plateau)
+    	* @param data is an array of one or more integers.
+    	*
+    	* @return the number of "plateaus" in the input data array.
+    	*/
+  	public static int numberOfPlateaus(int[] data){
 		return -7;
-  }
+  	}
 
 
 }
