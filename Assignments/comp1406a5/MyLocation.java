5d272ee2a0ca20f45c5bd06f071a804d33268560
mjhinek <jason.hinek@carleton.ca>
2019-08-08 11:17:15 -0400

    Update MyLocation.java

--- Assignments/comp1406a5/MyLocation.java
+++ Assignments/comp1406a5/MyLocation.java
@@ -1,5 +1,25 @@
 package comp1406a5;
 
-public class MyLOcation extends Location{
+public class MyLocation extends Location{
 
+  /* 
+   *   Leave these two conatructors as they are. We will use these to populate a world to test
+   *   your Gold methods
+   *
+   */
+	public MyLocation(int row, int col){ // makes a wall
+		super(row, col);
+	}
+	public Location(int row, int col, int gold){// not a wall
+		super(row, col, gold);
+	}
+  
+  
+  
+  /*
+   *  Add any attributes methods you need for your code. 
+   */
+  
+  
+  
 }
