1c17bd371e736064b41514011e0f4768e87165d6
mjhinek <jason.hinek@carleton.ca>
2019-08-08 03:01:21 -0400

    Update Gold.java

--- Assignments/comp1406a5/Gold.java
+++ Assignments/comp1406a5/Gold.java
@@ -3,10 +3,10 @@ package comp1406a5;
 public class Gold{
 
   /* a count of all the gold reachable from the start location in the given world */
-  public static int findGold(Location start, World world){return -1;}
+  public static int findGold(World world){return -1;}
 
   /* the computed perimieter of the reachable world from the start location in the given world */
-  public static int compuetPerimeter(Location start, World world){ return -1;}
+  public static int compuetPerimeter(World world){ return -1;}
 
 
 }
